e748f0f02083ba67963443ebb4936a38aa9d8b59
U-INTRA\mb264355 <mb264355@IS234555.intra.cea.fr>
2020-12-02 12:16:15 +0100

    setup project tree and initialized source files

---
+++ include/castor/castor_fftw.hpp
@@ -0,0 +1,25 @@
+/*
+
++-----------------------------------------------------------+
+|                                                           |
+|  (c) 2020 - PROPERTY OF MARC BAKRY - MIT license          |
+|                                                           |
++-----------------------------------------------------------+
+|                                                           |
+|       FILE       : castor_fftw.hpp                        |
+|       VERSION    : 0.1.0                                  |
+|       AUTHOR     : Marc Bakry                             |
+|       CREATION   : 29.11.2020                             |
+|       LAST MODIF : 29.11.2020                             |
+|       SYNOPSIS   : Wraps the well-known FFTW3 C library   |
+|                    within the 'castor' framework.         |
+|                                                           |
++-----------------------------------------------------------+
+
+ */
+#ifndef CASTOR_FFTW_HPP
+#define CASTOR_FFTW_HPP
+
+//
+
+#endif // CASTOR_FFTW_HPP
