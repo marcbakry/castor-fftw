637e96e6549d9acaf0236cdc37272dcf3fa4d42f
Marc <marc@marc-P15SM-A-SM1-A>
2020-12-06 23:25:14 +0100

    validated 1d transforms along matrix dimension

--- demo/demo_fftshift.cpp
+++ demo/demo_fftshift.cpp
@@ -12,6 +12,8 @@ int main()
     std::cout << "| TESTING CASTOR-FFTW: FFTFREQ/(I)FFTSHIFT |" << std::endl;
     std::cout << "+------------------------------------------+" << std::endl;
     //
+    std::cout << " - 1D version" << std::endl;
+    std::cout << " -- N odd" << std::endl;
     std::size_t N = 7;
     matrix<float> freq = fftw::fftfreq(N,1./N);
     disp(freq,1);
@@ -19,10 +21,27 @@ int main()
     disp(norm(fftw::ifftshift(fftw::fftshift(freq)) - freq,"inf"),1);
 
     std::cout << std::endl;
+    std::cout << " -- N even" << std::endl;
     N = 6;
     freq = fftw::fftfreq(N,1./N);
     disp(freq,1);
     disp(fftw::fftshift(freq),1);
     disp(norm(fftw::ifftshift(fftw::fftshift(freq)) - freq,"inf"),1);
+
+    //
+    std::cout << std::endl << " - 2D version: with dimension parameter" << std::endl;
+    std::size_t M = 7;
+    matrix<float> freq2 = zeros(M,N);
+    for(std::size_t i=0;i<M;++i)
+    {
+        for(std::size_t j=0; j<N; ++j) freq2(i,j) = freq(j);
+    }
+    disp(freq2,1);
+    disp(fftw::fftshift(freq2,2),1);
+    disp(norm(fftw::ifftshift(fftw::fftshift(freq2,2)),"inf"),1);
+
+    freq2 = transpose(freq2);
+    disp(fftw::fftshift(freq2,1),1);
+    disp(norm(fftw::ifftshift(fftw::fftshift(freq2,1)),"inf"),1);
     return EXIT_SUCCESS;
 }
--- include/castor/castor_fftwm.hpp
+++ include/castor/castor_fftwm.hpp
@@ -32,30 +32,171 @@ matrix<T> fftfreq(std::size_t n, T d)
 
 
 template<typename T>
-matrix<T> fftshift(matrix<T> const &A, int dim = 0)
+matrix<T> fftshift_1d(matrix<T> const &A)
 {
     std::size_t m=size(A,1), n=size(A,2);
     matrix<T> As = zeros(m,n);
-
-    //
-    if(m == 1 || n == 1) // one-dimensional case
+    std::size_t mn  = m*n;
+    std::size_t cnt = 0;
+    if(mn%2 == 0)
     {
-        std::size_t mn  = m*n;
-        std::size_t cnt = 0;
-        if(mn%2 == 0)
+        for(std::size_t i=mn/2; i<mn; ++i) As(cnt++) = A(i);
+        for(std::size_t i=0; i<mn/2; ++i)  As(cnt++) = A(i);
+    }
+    else
+    {
+        for(std::size_t i=(mn-1)/2+1; i<mn; ++i) As(cnt++) = A(i);
+        for(std::size_t i=0; i<(mn-1)/2+1; ++i)  As(cnt++) = A(i);
+    }
+    return As;
+}
+
+template<typename T>
+matrix<T> fftshift_2d1d(matrix<T> const &A, int dim=1)
+{
+    std::size_t m = size(A,1), n = size(A,2);
+    matrix<T> As = zeros(m,n);
+    if(dim == 1)
+    {
+        if(m%2 == 0)
         {
-            for(std::size_t i=mn/2; i<mn; ++i) As(cnt++) = A(i);
-            for(std::size_t i=0; i<mn/2; ++i)  As(cnt++) = A(i);
+            for(std::size_t j=0; j<n; ++j)
+            {
+                std::size_t cnt = 0;
+                for(std::size_t i=m/2; i<m; ++i) As(cnt++,j) = A(i,j);
+                for(std::size_t i=0; i<m/2; ++i) As(cnt++,j) = A(i,j);
+            }
         }
         else
         {
-            for(std::size_t i=(mn-1)/2+1; i<mn; ++i) As(cnt++) = A(i);
-            for(std::size_t i=0; i<(mn-1)/2+1; ++i)  As(cnt++) = A(i);
+            for(std::size_t j=0; j<n; ++j)
+            {
+                std::size_t cnt = 0;
+                for(std::size_t i=(m-1)/2+1; i<m; ++i) As(cnt++,j) = A(i,j);
+                for(std::size_t i=0; i<(m-1)/2+1; ++i) As(cnt++,j) = A(i,j);
+            }
+        }
+    }
+    else if(dim == 2)
+    {
+        if(n%2 == 0)
+        {
+            for(std::size_t j=0; j<m; ++j)
+            {
+                std::size_t cnt = 0;
+                for(std::size_t i=n/2; i<n; ++i) As(j,cnt++) = A(j,i);
+                for(std::size_t i=0; i<n/2; ++i) As(j,cnt++) = A(j,i);
+            }
+        }
+        else
+        {
+            for(std::size_t j=0; j<m; ++j)
+            {
+                std::size_t cnt = 0;
+                for(std::size_t i=(n-1)/2+1; i<n; ++i) As(j,cnt++) = A(j,i);
+                for(std::size_t i=0; i<(n-1)/2+1; ++i) As(j,cnt++) = A(j,i);
+            }
         }
     }
     else
     {
-        //
+        error(__FILE__, __LINE__, __FUNCTION__,"'dim' parameter should be 1 or 2.");
+    }
+    return As;
+}
+
+
+template<typename T>
+matrix<T> ifftshift_1d(matrix<T> const &As)
+{
+    std::size_t m=size(As,1), n=size(As,2);
+    matrix<T> A = zeros(m,n);
+    std::size_t mn  = m*n;
+    std::size_t cnt = 0;
+    if(mn%2 == 0)
+    {
+        for(std::size_t i=mn/2; i<mn; ++i) A(cnt++) = As(i);
+        for(std::size_t i=0; i<mn/2; ++i) A(cnt++) = As(i);
+    }
+    else
+    {
+        for(std::size_t i=(mn-1)/2; i<mn; ++i) A(cnt++) = As(i);
+        for(std::size_t i=0; i<(mn-1)/2; ++i) A(cnt++) = As(i);
+    }
+    return A;
+}
+
+template<typename T>
+matrix<T> ifftshift_2d1d(matrix<T> const &As, int dim = 0)
+{
+    std::size_t m = size(As,1), n = size(As,2);
+    matrix<T> A = zeros(m,n);
+    if(dim == 1)
+    {
+        if(m%2 == 0)
+        {
+            for(std::size_t j=0; j<n; ++j)
+            {
+                std::size_t cnt = 0;
+                for(std::size_t i=m/2; i<m; ++i) A(cnt++,j) = As(i,j);
+                for(std::size_t i=0; i<m/2; ++i) A(cnt++,j) = As(i,j);
+            }
+        }
+        else
+        {
+            for(std::size_t j=0; j<n; ++j)
+            {
+                std::size_t cnt = 0;
+                for(std::size_t i=(m-1)/2; i<m; ++i) A(cnt++,j) = As(i,j);
+                for(std::size_t i=0; i<(m-1)/2; ++i) A(cnt++,j) = As(i,j);
+            }
+        }
+    }
+    else if(dim == 2)
+    {
+        if(n%2 == 0)
+        {
+            for(std::size_t j=0; j<m; ++j)
+            {
+                std::size_t cnt = 0;
+                for(std::size_t i=n/2; i<n; ++i) A(j,cnt++) = As(j,i);
+                for(std::size_t i=0; i<n/2; ++i) A(j,cnt++) = As(j,i);
+            }
+        }
+        else
+        {
+            for(std::size_t j=0; j<m; ++j)
+            {
+                std::size_t cnt = 0;
+                for(std::size_t i=(n-1)/2; i<n; ++i) A(j,cnt++) = As(j,i);
+                for(std::size_t i=0; i<(n-1)/2; ++i) A(j,cnt++) = As(j,i);
+            }
+        }
+    }
+    else
+    {
+        error(__FILE__, __LINE__, __FUNCTION__,"'dim' parameter should be 1 or 2.");
+    }
+    return A;
+}
+
+
+
+// PUBLIC INTERFACE
+template<typename T>
+matrix<T> fftshift(matrix<T> const &A, int dim = 0)
+{
+    std::size_t m=size(A,1), n=size(A,2);
+    matrix<T> As= zeros(m,n);
+
+    //
+    if(m == 1 || n == 1) // one-dimensional case
+    {
+        return fftshift_1d(A);
+    }
+    else if(dim != 0)
+    {
+        return fftshift_2d1d(A,dim);
     }
     //
     return As;
@@ -71,27 +212,17 @@ matrix<T> ifftshift(matrix<T> const &As, int dim = 0)
     //
     if(m == 1 || n == 1)
     {
-        std::size_t mn  = m*n;
-        std::size_t cnt = 0;
-        if(mn%2 == 0)
-        {
-            for(std::size_t i=mn/2; i<mn; ++i) A(cnt++) = As(i);
-            for(std::size_t i=0; i<mn/2; ++i) A(cnt++) = As(i);
-        }
-        else
-        {
-            for(std::size_t i=(mn-1)/2; i<mn; ++i) A(cnt++) = As(i);
-            for(std::size_t i=0; i<(mn-1)/2; ++i) A(cnt++) = As(i);
-        }
+        return ifftshift_1d(As);
     }
-    else
+    else if(dim != 0)
     {
-        /* code */
+        return ifftshift_2d1d(As,dim);
     }
     return A;
 }
 
-        
+
+// END OF NAMESPACE        
 }
 }
 
